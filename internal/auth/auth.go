90fe5db0aefd5430193ff728642aa7922aadd909
gainax2k1 <gainax2k1@gmail.com>
2025-06-23 00:49:14 -0700

    corrected intentional broken code in auth.go

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -10,12 +10,12 @@ var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
-	authHeader := headers.Get("Authorzation") // removed "i" from Authorization
+	authHeader := headers.Get("Authorization")
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
 	}
 	splitAuth := strings.Split(authHeader, " ")
-	if len(splitAuth) < 2 || splitAuth[0] != "ApKey" { // removed "i" from ApiKey
+	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
 		return "", errors.New("malformed authorization header")
 	}
 
